68b873de7c46e48043d0d1fb4dadd026feb4efa8
Patrick Janssen <akiphtj@nus.edu.sg>
2018-07-18 11:21:12 +0800

    removed old files

--- src/typescript/_export.ts
+++ src/typescript/_export.ts
@@ -1,20 +1,2 @@
-// import * as calcOld from "./calc-old";
-// export {calcOld};
-
-// import * as consts from "./consts";
-// export {consts};
-
-// import * as rand from "./rand";
-// export {rand};
-
-// import * as round from "./round";
-// export {round};
-
-// import * as stats from "./stats";
-// export {stats};
-
-// import * as trig from "./trig";
-// export {trig};
-
 import * as calc from "./calc";
 export {calc};
